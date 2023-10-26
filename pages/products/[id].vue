8e95b51be916b893e85858cfa27ca74876beab07
GideonOdiokine <gidysmartodiokine@gmail.com>
2023-10-27 00:39:59 +0100

    added default and products layout

---
+++ pages/products/[id].vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore
+      assumenda, unde, cumque cum, aliquam provident dolorem totam libero
+      asperiores saepe enim eligendi excepturi porro aut minus. Distinctio quod
+      illum animi?
+    </p>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "products",
+});
+const { id } = useRoute().params;
+</script>
+
+<style lang="scss" scoped></style>
