da97303e9d6b74470557d01fd06c2d462b14817b
GideonOdiokine <gidysmartodiokine@gmail.com>
2023-10-27 01:54:23 +0100

    added custom error page

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,20 +1,26 @@
 <template>
   <div>
-    <p>Product details for {{ id }}</p>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore
-      assumenda, unde, cumque cum, aliquam provident dolorem totam libero
-      asperiores saepe enim eligendi excepturi porro aut minus. Distinctio quod
-      illum animi?
-    </p>
+    <!-- <ProductDetails :product="product" /> -->
+    <product-details :product="product" />
   </div>
 </template>
 
 <script setup>
+const { id } = useRoute().params;
+const uri = "https://fakestoreapi.com/products/" + id;
+
+//  fetch the products
+const { data: product } = await useFetch(uri, { key: id });
+
+if (!product.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: "Product not found",
+    fatal: true,
+  });
+}
+
 definePageMeta({
   layout: "products",
 });
-const { id } = useRoute().params;
 </script>
-
-<style lang="scss" scoped></style>
